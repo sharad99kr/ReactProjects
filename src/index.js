1c55a8ebeaa4c62e960c85f359ad119bbe10374f
Sharad Kumar <sharad9kumar@Sharads-MacBook-Pro.local>
2019-08-10 18:09:56 +0530

    [seasons app]completed section  5 : State in react components

--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
 //import react and react dom libraries
 import React from 'react';
 import ReactDom from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
 
 
 //create react component
-const App=()=>{
-    return <div className="ui container comments">
-        
-        <ApprovalCard>
-            <CommentDetail
-            author="sam"
-            timeAgo="today at 4"
-            content="nice post"
-            avatar={faker.image.avatar()}    
-            />
-        </ApprovalCard>
 
-        <ApprovalCard>
-            <CommentDetail
-            author="ram"
-            timeAgo="today at 5"
-            content="great post"
-            avatar={faker.image.avatar()}    
-            />
-        </ApprovalCard>
 
-        <ApprovalCard>
-            <CommentDetail
-            author="rahim"
-            timeAgo="today at 6"
-            content="what a post"
-            avatar={faker.image.avatar()}    
-            />
-        </ApprovalCard>
-        </div>;
-};
+class App extends React.Component{
 
+    constructor(props){
+        super(props);
+
+        this.state={
+            lat:null,
+            errorMessage:''
+        };
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>{
+                this.setState({lat:position.coords.latitude});
+            },
+            err=>{
+                this.setState({errorMessage:err.message});
+            }
+            
+        ); 
+    }
+
+    render(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>
+            Error : {this.state.errorMessage}
+            </div>;
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude : {this.state.lat}
+            </div>;
+        }
+        return <div>
+            Loading!
+            </div>;
+    }
+}
 
 //take creact component and show on screen
 ReactDom.render(
