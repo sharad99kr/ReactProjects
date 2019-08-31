d729a76545a86f9b76aa0c24e491942afbec4a40
Sharad Kumar <sharad9kumar@Sharads-MacBook-Pro.local>
2019-08-31 16:11:57 +0530

    [seasons app][Complete] 6:Understtanding lifecycle methods

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ class App extends React.Component{
         ); 
     }
 
-    render(){
+    renderContent(){
         if(this.state.errorMessage && !this.state.lat){
             return <div>
             Error : {this.state.errorMessage}
@@ -33,7 +33,15 @@ class App extends React.Component{
         if(!this.state.errorMessage && this.state.lat){
             return <SeasonDisplay lat={this.state.lat}/>
         }
-        return <Spinner/>;
+        return <Spinner message="Please accept location request"/>;
+    }
+
+    render(){
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
