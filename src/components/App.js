d6c130bfa85edb3d275ce69a45be25dc41b6330e
Sharad Kumar <sharad9kumar@Sharads-MacBook-Pro.local>
2019-09-29 15:44:40 +0530

    [Section 8 :  making api requests]Binding callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ import SearchBar from './SearchBar';
 import axios from 'axios';
 
 class App extends React.Component{
-
-    async onSearchSubmit(term){
-        console.log(term);
+    state = { images: [] };
+    onSearchSubmit = async(term) =>{
+        
         const response = await axios.get('https://api.unsplash.com/search/photos',{
             params:{ query: term},
             headers:{
@@ -14,6 +14,7 @@ class App extends React.Component{
              }   
 
         });
+        this.setState({images:response.data.results});
         console.log(response.data.results);
     }
 
@@ -21,6 +22,7 @@ class App extends React.Component{
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found:{this.state.images.length} images
             </div>);    
     }
 }
